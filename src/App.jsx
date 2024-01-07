cfcdf87f9c82d8aeed699995a18169247e92e35b
Emmanuel-Soempit <lukmmanuel@gmail.com>
2024-01-07 15:42:31 +0100

    ReadMe Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import SectionOne from './components/SectionOne'
+import SectionTwo from './components/SectionTwo'
+import SectionThree from './components/SectionThree'
+import FAQs from './components/FAQs'
+import Footer from './components/Footer'
 
 function App() {
  
@@ -10,7 +15,11 @@ function App() {
   return (
       <>
         <Header/>
-     
+        <SectionOne/>
+        <SectionTwo/>
+        <SectionThree/>
+        <FAQs/>
+        <Footer/>
       </>
   )
 }
